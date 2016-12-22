3d7afc3914c27b0af5fb7e58d9d5cf7b4f64fa1d
Andrew Wick <a.wick17@gmail.com>
2016-12-22 02:26:50 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  if !array.empty?
+    new_array = []
+    counter = 0
+    while counter < array.length
+      new_array << yield(array[counter])
+      counter += 1
+    end
+    return new_array
+  end
+end
